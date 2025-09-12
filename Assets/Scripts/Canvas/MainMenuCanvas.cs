3ba2114c9d5c5591d724c322cfa7a0cf26cef87a
Charles Wang <zwcharl@gmail.com>
2025-09-12 01:10:36 -0400

    hook functions up

--- Assets/Scripts/Canvas/MainMenuCanvas.cs
+++ Assets/Scripts/Canvas/MainMenuCanvas.cs
@@ -2,7 +2,5 @@ using UnityEngine;
 
 public class MainMenuCanvas : MonoBehaviour
 {
-    public static void StartGame() => GameManager.I.StartGame();
-
     public static void QuitGame() => Application.Quit();
 }
