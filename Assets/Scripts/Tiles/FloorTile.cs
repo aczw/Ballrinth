5ea414bfe5b3dc77580e4df7829bfd8ce7a69999
Charles Wang <zwcharl@gmail.com>
2025-09-11 22:39:18 -0400

    remove logs from tiles

--- Assets/Scripts/Tiles/FloorTile.cs
+++ Assets/Scripts/Tiles/FloorTile.cs
@@ -2,11 +2,5 @@ using UnityEngine;
 
 public class FloorTile : MonoBehaviour
 {
-    public virtual void OnCollisionEnter(Collision other) {
-        Debug.Log($"Entered floor tile w/ pos {transform.localPosition}!");
-
-        if (other.gameObject.CompareTag("Player")) {
-            Debug.Log("Ball entered this tile!");
-        }
-    }
+    public virtual void OnCollisionEnter(Collision other) { }
 }
