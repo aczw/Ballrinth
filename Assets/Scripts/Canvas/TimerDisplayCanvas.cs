530a4bca6b1a144eacdd14057a7e5082e2b5c6a1
Charles Wang <zwcharl@gmail.com>
2025-09-17 21:57:48 -0400

    add score text in-game

--- Assets/Scripts/Canvas/TimerDisplayCanvas.cs
+++ Assets/Scripts/Canvas/TimerDisplayCanvas.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class TimerDisplayCanvas : MonoBehaviour
 {
     [SerializeField] private TMP_Text currentTime;
+    [SerializeField] private TMP_Text currentScore;
 
     private void Update() {
         if (GameManager.I.GetGameState().status != GameState.Status.InGame) return;
 
         var time = GameManager.I.timer.GetCurrentTime();
+        var run = GameManager.I.GetRunState();
+
         currentTime.text = time.ToString("F2");
+        currentScore.text = run.stage.ToString();
     }
 }
